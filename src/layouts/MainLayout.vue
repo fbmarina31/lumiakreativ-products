4b86cc62baf1f5d5a38908c2b53652336d530490
fbmarina31 <fbmarina31@gmail.com>
2025-03-22 12:18:41 +0100

    setup quasar-project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="q-pa-sm">
+    <q-layout view="hHh Lpr lff">
+      <q-header :class="$q.dark.isActive ? 'bg-secondary' : 'bg-white'" style="height: 100px">
+        <q-toolbar class="q-pl-lg q-pb-sm"  style="height: 100px">
+          <img src="https://static.wixstatic.com/media/65c703_d6cca897e9b244f188ab20f8f5ac193f~mv2.png/v1/fill/w_124,h_100,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Untitled.png" class="q-mt-md q-pb-none" style="width: 80px"/>
+          <q-toolbar-title class="text-h6 text-grey-9 q-mt-xl q-pl-md">Willkommen auf der LuMia kreativ Produktseite</q-toolbar-title>
+          <q-btn flat round dense icon="language" color="grey-9" class="q-mt-xl q-pl-md"/>
+          <q-btn flat round dense icon="phone" color="grey-9" class="q-mt-xl q-pl-md" />
+          <q-btn flat round dense icon="mail" color="grey-9" class="q-mt-xl q-pl-md" />
+        </q-toolbar>
+      </q-header>
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="200"
+        :breakpoint="500"
+        bordered
+        :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+      >
+        <q-scroll-area class="fit">
+          <q-list>
+
+            <template v-for="(menuItem, index) in menuList" :key="index">
+              <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+                <q-item-section>
+                  {{ menuItem.label }}
+                </q-item-section>
+              </q-item>
+              <q-separator :key="'sep' + index"  v-if="menuItem.separator" />
+            </template>
+
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
+
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+
+      
+            <q-footer class="bg-grey-3">
+        <q-toolbar>
+          <q-toolbar-title class="text-caption text-grey-9 text-right">Raaf und Hochstatter GbR I Pfarrgasse 6 I 73460 Hüttlingen</q-toolbar-title>
+        </q-toolbar>
+      </q-footer>
+    </q-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const drawer = ref(false)
+const menuList = [
+  {
+    label: 'Neuheiten',
+    separator: true
+  },
+  {
+    label: 'Betonformen',
+    separator: false
+  },
+  {
+    label: 'Türkränze',
+    separator: true
+  },
+  {
+    label: 'Hochzeit',
+    separator: false
+  },
+  {
+    label: 'Kinder (Geburt, Geburtstag, Taufe)',
+    separator: false
+  },
+  {
+    label: 'Geburtstag',
+    separator: false
+  },
+    {
+    label: 'Sets zu verschiedenen Anlässen',
+    separator: true
+  },
+    {
+    label: 'Frühling/Ostern',
+    separator: false
+  },
+    {
+    label: 'Herbst',
+    separator: false
+  },
+    {
+    label: 'Winter/Weihnachten',
+  },
+]
+
+
+</script>
---
+++ src/pages/ProductsPage.vue
@@ -0,0 +1,39 @@
+<template>
+<q-page padding>
+    <q-banner class="text-grey-9 bg-white text-center">
+     Du bist auf der Suche nach einer Kleinigkeit für dich oder zum verschenken? Dann wirst du hoffentlich bei uns fündig.<br> Gerne erstellen und personalisieren wir auch etwas nach deinen Wünschen. <br>Melde Dich gerne bei uns über die oben rechts aufgezeigten Kontaktmöglichkeiten. 
+    </q-banner>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card" flat bordered style="width: 300px">
+      <img src="https://static.wixstatic.com/media/65c703_0e621d2ad076401c9fbd77a2dd589639~mv2.jpg/v1/fill/w_575,h_500,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/20240217_125721300_iOS.jpg">
+      <q-list>
+          <q-item-section>
+            <q-item-label class="text-h5 bg-grey-3 q-pa-xs text-center">Set Osterschale</q-item-label>
+            <div class="row q-pa-xs q-mt-sm">
+              <div class="q-px-xs">Nr.</div>
+              <div class="q-px-xs text-weight-bold">120</div>
+            </div>
+            <div class="row q-pa-xs">
+              <div class="q-px-xs">Preis</div>
+              <div class="q-px-xs text-weight-bold">9,00€</div>
+            </div>
+            <div class="row q-pa-xs">
+              <div class="q-px-xs">Maße</div>
+              <div class="q-px-xs text-weight-bold">10 x 5 x 3 cm</div>
+            </div>
+            <div class="row q-pa-xs">
+              <div class="q-px-xs">Beschreibung</div>
+              <div class="q-px-xs text-weight-bold">Das Set gibt es wahlweise in weiß oder grau</div>
+            </div>
+          </q-item-section>
+      </q-list>
+    </q-card>
+    </div>
+
+        </q-page> 
+</template>
+
+<script setup lang="ts">
+
+
+</script>
