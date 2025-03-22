93bf1e21f5711c404ce773e61d81969aad07993e
fbmarina31 <fbmarina31@gmail.com>
2025-03-22 12:38:19 +0100

    git hub deployment

--- quasar.config.ts
+++ quasar.config.ts
@@ -56,7 +56,7 @@ export default defineConfig((ctx) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/lumiakreativ-products',
       // analyze: true,
       // env: {},
       // rawDefine: {}
